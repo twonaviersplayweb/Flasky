940f6b9bb90a1ace5a4cb783eeaccedbeb08838f
setsuna0728 <setsuna0728@hotmail.com>
2015-09-22 15:52:58 +0800

    database

--- hello.py
+++ hello.py
@@ -1,20 +1,54 @@
+import os
 from flask import Flask, render_template, session, url_for, redirect, flash
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
+from flask.ext.migrate import Migrate, MigrateCommand
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLQLCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators = [Required()])
     submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), unique = True, index = True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
     
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+def make_shell_context():
+    return dict(app = app, db = db, User = User, Role = Role)
+manager.add_command('shell', Shell(make_context = make_shell_context))
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -28,12 +62,17 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name = session.get('name'))
+         user = User.query.filter_by(username = form.name.data).first()
+         if user is None:
+            user = User(username = form.name.data)
+            db.session.add(user)
+            session['known'] = False
+         else:
+                session['known'] = True   
+         session['name'] = form.name.data
+         form.name.data = ''
+         return redirect(url_for('index'))
+    return render_template('index.html', form=form, name = session.get('name'), known = session.get('known', False))
 
 if __name__ == '__main__':
     manager.run()
