768f0ecd698dcc823cf361ed2bff8ad3140d1730
setsuna0728 <setsuna0728@hotmail.com>
2015-09-23 21:59:24 +0800

    Chapter 7

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.mail import Mail, Message
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    db.init_app(app)
+
+    from .mian import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
